8d0458712ad86670bfe72d9c4428b89f3c56512b
Jim Thompson <jimthompson5802@gmail.com>
2025-11-09 09:32:54 -0500

    Add run_all.sh script to automate branch switching and program execution

---
+++ run_all.sh
@@ -0,0 +1,17 @@
+git checkout main
+ls -lh
+./run_programs.sh
+
+git checkout branch1
+ls -lh
+./run_programs.sh
+
+git checkout branch2
+ls -lh
+./run_programs.sh
+
+git checkout branch3
+ls -lh
+./run_programs.sh
+
+git checkout main
